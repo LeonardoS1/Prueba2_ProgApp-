0be94db69501a3d050f893e38094e9c2421997e7
MauroB <97251875+Traceurflipxd@users.noreply.github.com>
2022-10-16 19:17:50 -0300

    Primera modificacion Local Storage

--- src/app/services/db.service.ts
+++ src/app/services/db.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
+import { Console } from 'console';
 
 @Injectable({
   providedIn: 'root'
@@ -7,9 +9,27 @@ import { Router } from '@angular/router';
 export class DbService {
   validador: boolean = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,private sqlite:SQLite) { 
 
-  canActivate(){
+     this.sqlite.create({
+      name:'datos.db'
+     }).then((db:SQLiteObject)=>
+     db.executeSql('CREATE TABLE IF NOT EXIST USUARIO(MAIL(VARCHAR(75),CONTRASENA(VARCHAR(30))',[]).then(()=>{
+      console.log('TABLA CREADA OK');
+    
+    }).catch(e =>{
+      console.log('TABLA NO OK');
+    }).catch(e =>{
+    console.log('BASE DE DATOS OK');
+  }
+
+}    
+
+
+  
+  
+
+  canActivate(): boolean{
     if(this.validador){
       return true;
     }else {
